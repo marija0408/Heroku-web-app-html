98f09b259b521698b33211b8e4c427eb9e996967
Marija <marija0408@gmail.com>
2022-10-25 19:16:55 +0200

    changes

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 import numpy as np
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template, jsonify
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def home():
-    return render_template('index.html')
+    data = 'hello world'
+    return jsonify({'data':data})
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict/')
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    years = request.args.get('years')
+    float_features = [float(years)]
+    final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
 
+    input_feature = float_features[0]
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='House price should be $ {}'.format(output))
+    return jsonify({'Salary': str(output)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port=9874)
